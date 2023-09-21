9976908f40efbccd8d3bae2446f5b00d68d6d2e1
irinapi17 <irinapi17@yahoo.com>
2023-09-20 19:01:48 -0700

    My day 10  Commit

---
+++ src/day10_if_statements/IfWithoutBrackets.java
@@ -0,0 +1,31 @@
+package day10_if_statements;
+
+public class IfWithoutBrackets {
+    public static void main(String[] args) {
+
+        if(4 > 2) {
+            System.out.println("Hello");
+        }
+// Single if ststement with Brackets --- which has one statement
+        //semicolon ends if statement if no brackets
+        System.out.println("bye");
+
+
+
+
+
+        if(4 > 1) {
+            System.out.println("Hello");
+
+            //if with 2 statements
+            System.out.println("bye");
+
+        }
+
+        if(5 > 1)
+            System.out.println("Hello");
+        // if statement without brackets with one statement
+        //semicolon ends if statement if no brackets
+        System.out.println("bye");
+    }
+}
