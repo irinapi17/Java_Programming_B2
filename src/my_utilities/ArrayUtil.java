96b1d59313ca96bb02a73e21be668351502b9ad2
irinapi17 <irinapi17@yahoo.com>
2023-10-28 12:08:33 -0700

    My day 27  Commit

--- src/my_utilities/ArrayUtil.java
+++ src/my_utilities/ArrayUtil.java
@@ -1,5 +1,6 @@
 package my_utilities;
 
+import java.util.Arrays;
 public class ArrayUtil {
 
     public static int sumOfArray ( int [] arr) {
@@ -53,4 +54,39 @@ public class ArrayUtil {
     }
 
 
+
+    public static int indexOf (int [] numArr, int num) {
+        for (int i = 0; i < numArr.length; i++) {
+            if ( numArr[i] == num) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+
+    public static int indexOf (String [] str, String word) {
+        for (int i = 0; i < str.length; i++) {
+            if ( str[i].equals(word)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static int [] addElementInArr (int [] originalArr, int num) {
+        int [] newArr = Arrays.copyOf(originalArr, originalArr.length+1 ); // [1, 3, 5, 2, 123, 234, _ ]
+        newArr[newArr.length-1] = num;
+        return newArr;
+    }
+
+
+    public static String [] addElementInArr (String [] originalArr, String str) {
+        String [] newArr = new String [originalArr.length + 1];
+        for (int i = 0; i < originalArr.length; i++) {
+            newArr[i] = originalArr [i];
+        }
+        newArr [newArr.length - 1] = str;
+        return newArr;
+    }
 }
